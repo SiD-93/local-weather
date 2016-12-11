b05d39bb9d6673a9d005d392013d3984b3df03ff
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-12-11 22:39:59 +0530

    First commit

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import WeatherCard from '../components/WeatherCard';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <WeatherCard />
+    )
+  }
+}
+
+export default AppContainer;
