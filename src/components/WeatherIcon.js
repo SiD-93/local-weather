d8cb562e879076117440ab61e75ddae65026cce1
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2017-01-19 11:30:06 +0530

    minor test fix and time of day

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function WeatherIcon(props) {
+  let timeOfDay = (new Date().getHours() > 19) ? 'night' : 'day';
   return (
-    <p className={`wi wi-owm-${props.weatherIconCode} text-center weatherIcon`}></p>
+    <p className={`wi wi-owm-${timeOfDay}-${props.weatherIconCode} text-center weatherIcon`}></p>
   )
 }
 
